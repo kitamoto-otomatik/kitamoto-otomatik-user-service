1abb678a35755d88f8a2ed5f810a8cc1c5c50d36
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2021-10-18 02:49:34 -0400

    Minor cleanup

--- src/main/java/com/demo/controller/AccountController.java
+++ src/main/java/com/demo/controller/AccountController.java
@@ -4,8 +4,6 @@ import com.demo.model.AccountStatusResponse;
 import com.demo.orchestrator.AccountStatusOrchestrator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -17,7 +15,7 @@ public class AccountController {
     private AccountStatusOrchestrator orchestrator;
 
     @GetMapping("/{username}")
-    public ResponseEntity<AccountStatusResponse> getAccountStatus(@PathVariable String username) {
-        return new ResponseEntity<>(new AccountStatusResponse(orchestrator.orchestrate(username)), HttpStatus.OK);
+    public AccountStatusResponse getAccountStatus(@PathVariable String username) {
+        return new AccountStatusResponse(orchestrator.orchestrate(username));
     }
 }
