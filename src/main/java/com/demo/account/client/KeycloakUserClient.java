2f3655db7b55225a162c481ca8851c1727104ff7
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2021-10-24 03:36:52 -0400

    Added KeycloakUserClientTest

--- src/main/java/com/demo/account/client/KeycloakUserClient.java
+++ src/main/java/com/demo/account/client/KeycloakUserClient.java
@@ -9,15 +9,15 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import javax.annotation.PostConstruct;
 import java.util.Arrays;
 import java.util.List;
 
 @Component
 public class KeycloakUserClient {
     private static final String ERROR_MESSAGE = "Could not get Keycloak users";
-
     private final KeycloakTokenClient tokenService;
-
+    private WebClient webClient;
     @Value("${keycloak.host}")
     private String host;
 
@@ -29,10 +29,15 @@ public class KeycloakUserClient {
         this.tokenService = tokenService;
     }
 
-    public Mono<List<KeycloakUser>> getUserListByUsername(String username) {
-        return tokenService.getKeycloakToken().flatMap(token -> WebClient.builder()
+    @PostConstruct
+    public void postConstruct() {
+        this.webClient = WebClient.builder()
                 .baseUrl(host)
-                .build()
+                .build();
+    }
+
+    public Mono<List<KeycloakUser>> getUserListByUsername(String username) {
+        return tokenService.getKeycloakToken().flatMap(token -> webClient
                 .get()
                 .uri(e -> e.path(usersEndpoint).queryParam("username", username).build())
                 .headers(httpHeaders -> httpHeaders.setBearerAuth(token))
