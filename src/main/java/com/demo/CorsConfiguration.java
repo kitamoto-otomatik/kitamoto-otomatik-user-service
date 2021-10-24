3fe054adb77a97d441653d39dc909a3bc1edcd47
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2021-10-24 19:17:40 -0400

    Allowed CORS

---
+++ src/main/java/com/demo/CorsConfiguration.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+public class CorsConfiguration implements WebFluxConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .maxAge(3600);
+    }
+}
