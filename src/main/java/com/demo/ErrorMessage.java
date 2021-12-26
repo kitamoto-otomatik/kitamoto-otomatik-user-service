cdc9fb0c28561d25d0195f8cae015a3355322c59
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2021-12-25 20:09:44 -0500

    Minor code cleanup

--- src/main/java/com/demo/ErrorMessage.java
+++ src/main/java/com/demo/ErrorMessage.java
@@ -7,7 +7,6 @@ public class ErrorMessage {
     public static final String KEYCLOAK_USER_UPDATE_ERROR_MESSAGE = "An error was encountered while trying to update an account. ";
     public static final String KEYCLOAK_USER_ACTIVATION_ERROR_MESSAGE = "An error was encountered while trying activate an account. ";
     public static final String EMAIL_SENDING_ERROR = "An error was encountered while trying to send an email. ";
-    public static final String NON_UNIQUE_USERNAME_FOUND_ERROR_MESSAGE = "Multiple users with the same username was found. ";
     public static final String USERNAME_DOES_NOT_EXIST_ERROR_MESSAGE = "Username not found. ";
     public static final String USERNAME_IS_ALREADY_TAKEN_ERROR_MESSAGE = "Username is already taken. ";
     public static final String ACCOUNT_IS_ALREADY_ACTIVATED_ERROR_MESSAGE = "Account is already activated. ";
--- src/main/java/com/demo/account/client/KeycloakTokenClient.java
+++ src/main/java/com/demo/account/client/KeycloakTokenClient.java
@@ -1,8 +1,5 @@
 package com.demo.account.client;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public interface KeycloakTokenClient {
     String getKeycloakToken();
 }
